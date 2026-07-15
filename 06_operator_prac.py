d095294ec74da59ffaf5b8f9d356983f55b2a9c4
Min-Jun-Kang <zhfeps123@naver.com>
2026-07-15 17:53:32 +0900

    연산자 실습

---
+++ 06_operator_prac.py
@@ -0,0 +1,23 @@
+# 실습 1번
+
+a = 17
+b = 5
+
+print(a + b, a - b, a * b, a / b, a // b, a % b, a**b)
+
+
+# 실습 2번
+
+score_1 = 95
+score_2 = 80
+score_3 = 88
+
+print("평균:", round((score_1 + score_2 + score_3) / 3, 2))
+
+length = 8  # 정사각형 한 변의 길이
+print("정사각형 넓이:", length**2)
+
+width = 7  # 가로
+length = 6  # 세로
+height = 8  # 높이
+print("직육면체의 부피:", width * length * height)
