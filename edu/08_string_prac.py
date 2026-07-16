cb8ca476914807c0f50eb0ec4813b3aa51da88df
Min-Jun-Kang <zhfeps123@naver.com>
2026-07-16 16:48:40 +0900

    study : 인덱싱, 슬라이싱 실습

---
+++ edu/08_string_prac.py
@@ -0,0 +1,43 @@
+# # 실습 :
+# # 결과 값 :
+# # 설비 : PUMP_A > str
+# # 상태 : 정상 > str
+# # 가동 : 1200 > int
+# # 점검 : 2026-07-16 > str
+
+# name = input("설비 이름을 적어주세요 > ")
+# status = input("설비 상태를 적어주세요 > ")
+# time = int(input("설비 가동 시간을 적어주세요 > "))
+# date = input("설비 점검 시간을 적어주세요 > ")
+
+# print("설비:", name, "\n상태:", status, "\n가동:", time, "\n점검:", date)
+
+# # 실습 [:n] start 생략
+
+# word = "temp_sensor"
+
+# print("4글자만 추출 >", word[:4])
+
+# # 실습 [n:] end 생략
+
+# word = "temp_sensor"
+
+# print("5번부터 끝까지 추출 >", word[5:])
+
+# # 실습 [-n:] 음수 슬라이싱 (뒤n글자)
+
+# word = "sensor_01"
+
+# print("뒤 2글자 출력 >", word[-2:])
+
+# # 실습 step으로 건너뛰기
+
+# word = "PYTHON"
+
+# print("두 칸씩 건너뛴 글자 출력 >", word[::2])
+
+# # 실습 문자열 뒤집기
+
+# word = "PYTHON"
+
+# print("문자열 뒤집어서 출력 >", word[::-1])
