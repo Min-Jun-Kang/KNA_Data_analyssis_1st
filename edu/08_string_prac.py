9c7d8f3a86cf3b1aa352df5497ea04ad8a312229
Min-Jun-Kang <zhfeps123@naver.com>
2026-07-27 17:48:34 +0900

    문자열 실습

--- edu/08_string_prac.py
+++ edu/08_string_prac.py
@@ -52,3 +52,111 @@
 
 # word = "a,b,c,d"
 # print("문자열에 포함된 ,의 개수: >", word.count(","))
+
+
+# # 실습 / startswith(), endswith()
+
+# str = "sensor_log.csv"
+
+# print(str.startswith("sensor"))
+# print(str.endswith(".csv"))
+
+# # 실습 / 대문자 바꾸기
+
+# word = "ready"
+# WORD = word.upper()
+# print(WORD)
+
+# # 실습 / 소문자 바꾸기
+
+# WARN = "WARNING"
+
+# warn = WARN.lower()
+
+# print(warn)
+
+# # 실습 / capitalize(), title() 이용
+
+# name = "kang min jun"
+# test = "I'm full"
+
+# # 실습 / 대문자 소문자 확인
+
+# word_1 = "ABC"
+# word_2 = "abc"
+# word_3 = "Abc"
+
+# print(word_1.isupper())  # True
+# print(word_2.islower())  # True
+# print(word_3.isupper())  # False
+# # print(word_3.islower()) # False
+
+# # 실습 / 파일명 규칙 한 번에 점검하기
+
+# file = "Sensor_LOG.CSV"
+
+# file_lower = file.lower()
+
+
+# print(file.startswith("sensor"))
+# print(file.endswith(".csv"))
+# print(file_lower.startswith("sensor"))
+# print(file_lower.endswith(".csv"))
+
+
+# # 실습 / 문자열 형태 바꿔서 출력하기
+
+# str = "   Warning      "
+
+# # 출력 예상, 체이닝 해보기
+
+# str1 = str.lower()
+# str2 = str.strip().lower()
+
+# print("[" + str1 + "]")
+# print("[" + str2 + "]")
+
+# # 실습 / 쉼표 기준으로 나누기
+
+# word = "a,b,c,d"
+# word_list = word.split(",")
+# print(word_list)
+
+# # 실습 / join 이용해서 리스트 합치기
+
+# list1 = ["2025", "01", "15"]
+# print("-".join(list1))
+
+# # 실습 / python -> pyThon으로 바꾸기
+
+# word = "python"
+
+# # strip + capitalize
+# print(word[:2] + word.strip("py").capitalize())
+
+# # strip + title
+# print(word[:2] + word.strip("py").title())
+
+# # replace
+# print(word.replace("t", "T"))
+
+# # 슬라이싱 + upper 사용
+# print(word[:2] + word[2].upper() + word[3:])
+
+# # 인덱싱으로 글자 하나씩 연결
+# print(word[0] + word[1] + word[2].upper() + word[3] + word[4] + word[5])
+
+# # split, join 이용
+# print("T".join(word.split("t")))
+# print(word[2].upper().join(word.split("t")))
+
+# # 실습 / 구분자 통째로 바꾸기
+
+# date = "2025/01/15"
+# date_list = date.split("/")  # ['2025', '01', '15']
+# print("-".join(date_list))  # 2025-01-15
+
+# # 실습 / csv 한 줄에서 값 꺼내 정리하기
+
+# senten = "1, NORMAL ,25.3"
+# print(senten.split(",")[1].strip().lower())
